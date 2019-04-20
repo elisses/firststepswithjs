c40242e8e85f2b6c9bd698491fbbbdc8657f82be
elisses <fernandalisses@gmail.com>
2019-04-21 00:39:16 +0200

    Declaration of variables

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('arquivo carregado')
+var name = 'bruno'
+var age = 25
+var weight = 85.2
+var adult =  true
+//trocando o tipo
+adult = 'false'
+//typeof you now type variable
+console.log(age, typeof age)
+console.log(weight, typeof weight)
+console.log(adult, typeof adult)
+console.log(name, typeof name)
+
+
+
